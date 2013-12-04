f4f5b958cf0cb6bd9a0bf3670fb186bb1de9f790
Bede Constantinides <bedeabc@gmail.com>
2013-12-04 11:44:19 +0000

    Added overall percentage change to output

--- monitor_prices.py
+++ monitor_prices.py
@@ -8,13 +8,12 @@ alert_threshold = float(sys.argv[1])
 notify_threshold = float(sys.argv[2])
 currency_pair_name = ' ' + 'LTC/BTC'
 currency_pair_url = 'https://btc-e.com/api/2/ltc_btc/ticker'
-polling_interval = 5
+polling_interval = 2
 initial_price = None
 current_price = None
 last_price = None
 
 def alert(direction=None):
-	global last_price
 	if direction is 'up':
 		print u'\u2b06 ' + str(current_price) + currency_pair_name +  ' - alert'
 	elif direction is 'down':
@@ -22,17 +21,20 @@ def alert(direction=None):
 	for i in range(0,5):
 		sys.stdout.write('\a')
 		sys.stdout.flush()
-	last_price = current_price
 
 def notify(direction):
-	global last_price
+	if direction is not 'none':
+		percent_change = round(((current_price-initial_price)/initial_price)*100, 2)
+		if percent_change > 0:
+			percent_change = ' +' + str(percent_change) + '%'
+		else:
+			percent_change = ' ' + str(percent_change) + '%'
 	if direction is 'up':
-		print u'\u2b06 ' + str(current_price) + currency_pair_name
+		print u'\u2b06 ' + str(current_price) + currency_pair_name + percent_change
 	elif direction is 'down':
-		print u'\u2b07 ' + str(current_price) + currency_pair_name
+		print u'\u2b07 ' + str(current_price) + currency_pair_name + percent_change
 	elif direction is 'none':
-		print str(initial_price) + currency_pair_name
-	last_price = current_price
+		print 'Starting at ' + str(initial_price) + currency_pair_name
 
 def fetch_initial_price():
 	global initial_price
@@ -41,7 +43,6 @@ def fetch_initial_price():
 	initial_price = float(r.json()['ticker']['last'])
 	last_price = initial_price
 	notify('none')
-	time.sleep(polling_interval)
 
 def check_current_price():
 	global current_price
@@ -50,14 +51,19 @@ def check_current_price():
 	current_price = float(r.json()['ticker']['last'])
 	if current_price > initial_price*(1+alert_threshold):
 		alert('up')
+		last_price = current_price
 	elif current_price < initial_price*(1-alert_threshold):
 		alert('down')
+		last_price = current_price
 	elif current_price > last_price*(1+notify_threshold):
 		notify('up')
+		last_price = current_price
 	elif current_price < last_price*(1-notify_threshold):
 		notify('down')
-		
+		last_price = current_price
+
 fetch_initial_price()
+time.sleep(polling_interval)
 while True:
 	check_current_price()
 	time.sleep(polling_interval)
